341c353ae0459e00800ea6daabd0becb1d183258
saikumar7817 <saikumar.2154@gmail.com>
2025-09-21 16:49:15 +0000

    13

--- botkit/legal/app.js
+++ botkit/legal/app.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 // Define the port the server will listen on
-const port = 3000;
+const port = 5000;
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
--- botkit/tap/app.js
+++ botkit/tap/app.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 // Define the port the server will listen on
-const port = 3000;
+const port = 7000;
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
